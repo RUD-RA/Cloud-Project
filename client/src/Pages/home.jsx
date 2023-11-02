2ab123b5bb42aafba108fae38d881e189ab0a46e
Ithiraj Hazarika <111444971+ithiraj-hazarika-07@users.noreply.github.com>
2023-11-02 20:07:10 +0530

    Updated Files

--- client/src/Pages/home.jsx
+++ client/src/Pages/home.jsx
@@ -1,145 +1,154 @@
 import React, { useEffect, useState } from "react";
 import { BsBellFill } from "react-icons/bs";
 import {
-  AiFillMessage,
-  AiOutlineClose,
+    AiFillMessage,
+    AiOutlineClose,
 } from "react-icons/ai";
 import { RiArrowDropDownLine } from "react-icons/ri";
 import { BiSearchAlt2 } from "react-icons/bi";
 import { FiMoreVertical } from "react-icons/fi";
 import { BsDownload } from "react-icons/bs";
 import "./homestyle.css";
-
-import { handleUploadClick } from "./uploadFile.jsx";
+import { handleUploadClick } from "./uploadFile.jsx"
 
 function Home() {
-  const [imageUrls, setImageUrls] = useState([]);
-  const myArray = ["60vh", "30vh", "35vh", "50vh", "40vh", "60vh"];
-  function getRandomElement(arr) {
-    const randomIndex = Math.floor(Math.random() * arr.length);
-    return arr[randomIndex];
-  }
-  const randomElement = getRandomElement(myArray);
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const response = await fetch(
-          "https://2cs1fcufue.execute-api.us-west-2.amazonaws.com/getallimage"
-        );
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
+    const [imageUrls, setImageUrls] = useState([]);
+    const [imagePreview, setImagePreview] = useState(null);
+    const [file, setFile] = useState([]);
+    const myArray = ["60vh", "30vh", "35vh", "50vh", "40vh", "60vh"];
+    function getRandomElement(arr) {
+        const randomIndex = Math.floor(Math.random() * arr.length);
+        return arr[randomIndex];
+    }
+    const randomElement = getRandomElement(myArray);
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await fetch(
+                    "https://2cs1fcufue.execute-api.us-west-2.amazonaws.com/getallimage"
+                );
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                const data = await response.json();
+                setImageUrls(data);
+            } catch (error) {
+                console.error("Error:", error);
+            }
         }
-        const data = await response.json();
-        setImageUrls(data);
-      } catch (error) {
-        console.error("Error:", error);
-      }
-    }
 
-    fetchData();
-  }, []);
+        fetchData();
+    }, []);
 
-  const [isopen, setIsopen] = useState(false);
-  const openmenu = () => {
-    if (isopen === false) {
-      setIsopen(true);
-    } else {
-      setIsopen(false);
-    }
-  };
+    const [isopen, setIsopen] = useState(false);
+    const openmenu = () => {
+        if (isopen === false) {
+            setIsopen(true);
+        } else {
+            setIsopen(false);
+        }
+    };
 
-  return (
-    <div>
-      <nav className="mynav">
-        <div style={{ marginLeft: "15px" }}>
-          <img
-            className="logo"
-            src="https://img.icons8.com/bubbles/100/stack-of-photos.png"
-            alt="website-logo"
-          />
-        </div>
-        <div className="nav-ele">Home</div>
-        <div className="nav-ele">Explore</div>
-        <div className="nav-ele">
-          <div className="create-ele " onClick={openmenu}>
-            Create <RiArrowDropDownLine />
-          </div>
-        </div>
-        <div className="navsearch">
-          <div className="search-icon">
-            <BiSearchAlt2 />
-          </div>
-          <input type="text" placeholder="Search" />
-        </div>
-        <div className="nav-ele icons">
-          <BsBellFill />
-        </div>
-        <div className="nav-ele icons">
-          <AiFillMessage />
-        </div>
-        <div className="nav-ele icons">
-          <div>
-            <img
-              className="logo1"
-              src="https://img.icons8.com/ios-filled/50/user-male-circle.png"
-              alt="user-male-circle"
-            />
-          </div>
-        </div>
-        <div className="nav-ele last">
-          <RiArrowDropDownLine />
-        </div>
-      </nav>
-      <div className="body-con">
-        <div className="pinwrapper">
-          {imageUrls.map((url, index) => (
-            <div className="pin" style={{ height: "40vh" }} key={index}>
-              <img src={url} alt="Display" />
-              <div className="overlay">
-                <div className="title">
-                  {url.split("/").pop().split(".").slice(0, -1).join(".")}
+    const handleFileSelect = (e) => {
+        const selectedFile = e.target.files[0];
+        setFile(selectedFile);
+        if (selectedFile) {
+            setImagePreview(URL.createObjectURL(selectedFile));
+        }
+    };
+
+    return (
+        <div>
+            <nav className="mynav">
+                <div style={{ marginLeft: "15px" }}>
+                    <img
+                        className="logo"
+                        src="https://img.icons8.com/bubbles/100/stack-of-photos.png"
+                        alt="website-logo"
+                    />
                 </div>
-                <div className="options">
-                  <a href={url} target="_blank" rel="noreferrer">
-                    <BsDownload className="download" />{" "}
-                  </a>
-                  <FiMoreVertical />
+                <div className="nav-ele">Home</div>
+                <div className="nav-ele">Explore</div>
+                <div className="nav-ele">
+                    <div className="create-ele " onClick={openmenu}>
+                        Create <RiArrowDropDownLine />
+                    </div>
                 </div>
-              </div>
+                <div className="navsearch">
+                    <div className="search-icon">
+                        <BiSearchAlt2 />
+                    </div>
+                    <input type="text" placeholder="Search" />
+                </div>
+                <div className="nav-ele icons">
+                    <BsBellFill />
+                </div>
+                <div className="nav-ele icons">
+                    <AiFillMessage />
+                </div>
+                <div className="nav-ele icons">
+                    <div>
+                        <img
+                            className="logo1"
+                            src="https://img.icons8.com/color/96/checked-user-male--v1.png"
+                            alt="checked-user-male--v1"
+                        />
+                    </div>
+                </div>
+                <div className="nav-ele last">
+                    <RiArrowDropDownLine />
+                </div>
+            </nav>
+            <div className="body-con">
+                <div className="pinwrapper">
+                    {imageUrls.map((url, index) => (
+                        <div className="pin" style={{ height: "40vh" }} key={index}>
+                            <img src={url} alt="Display" />
+                            <div className="overlay">
+                                <div className="title">{url.split('/').pop().split('.').slice(0, -1).join('.')}</div>
+                                {console.log(randomElement)}
+                                <div className="options">
+                                    <a href={url} target='_blank' rel='noreferrer'><BsDownload className='download' /> </a><FiMoreVertical />
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
-          ))}
+            {isopen && (
+                <div className="createpannel">
+                    <div className="left">
+                        <div className="upld-con">
+                            <input
+                                type="file"
+                                id="fileInput"
+                                style={{ display: "none" }}
+                                accept="image/*"
+                                onChange={handleFileSelect}
+                            />
+                            <label htmlFor="fileInput" style={{ fontFamily: "Bree Serif" }}>
+                                Select an image
+                            </label>
+                            {imagePreview && <img src={imagePreview} alt="Preview" className="image-preview" />}
+                        </div>
+                    </div>
+                    <div className="right">
+                        <div className="close" onClick={openmenu}>
+                            <AiOutlineClose />
+                        </div>
+                        <div className="ins-con">
+                            <label className="title1">Title</label>
+                            <input type="text" className="inputs" />
+                            <br />
+                            <button onClick={() => {
+                                handleUploadClick(file)
+                            }}>Submit</button>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
-      </div>
-      {isopen && (
-        <div className="createpannel">
-          <div className="left">
-            <div className="upld-con">
-              <input
-                type="file"
-                id="fileInput"
-                style={{ display: "none" }}
-                accept="image/*"
-              />
-              <label htmlFor="fileInput" style={{ fontFamily: "Bree Serif" }}>
-                Select an image
-              </label>
-            </div>
-          </div>
-          <div className="right">
-            <div className="close" onClick={openmenu}>
-              <AiOutlineClose />
-            </div>
-            <div className="ins-con">
-              <label className="title1">Title</label>
-              <input type="text" className="inputs" id="key" />
-              <br />
-              <button onClick={handleUploadClick}>Submit</button>
-            </div>
-          </div>
-        </div>
-      )}
-    </div>
-  );
+    );
 }
 
 export default Home;
--- client/src/Pages/uploadFile.jsx
+++ client/src/Pages/uploadFile.jsx
@@ -1,55 +1,77 @@
 import AWS from "aws-sdk";
-
-export function handleUploadClick() {
-  AWS.config.update({
-    accessKeyId: 'YOUR_AWS_ACCESS_KEY_ID',
-    secretAccessKey: 'AWS_SECRET_ACCESS_KEY',
-    region: 'YOUR_AWS_REGION'
-  });
-
-  const fileInput = document.getElementById("fileInput");
-  let filename = document.getElementById("key").value;
-
-  if (fileInput.files.length > 0) {
-    const selectedFile = fileInput.files[0];
-    uploadFile(selectedFile);
-  } else {
-    alert("Select File and try again!");
-  }
-
-  function uploadFile(selectedFile) {
-    const s3 = new AWS.S3();
-    let type = "";
-
-    if (selectedFile.type === "image/jpeg") {
-      type = ".jpg";
-    } else if (selectedFile.type === "image/png") {
-      type = ".png";
-    } 
-    // else if (selectedFile.type === "image/svg+xml") {
-    //   type = ".svg";
-    // } 
-    else if (selectedFile.type === "image/gif") {
-      type = ".gif";
-    }
-
-    if(filename === ''){
-      filename = selectedFile.name;
-    }
-
-    const params = {
-      Bucket: "imageproject101",
-      Key: filename + type,
-      Body: selectedFile,
-      ContentType: selectedFile.type
-    };
-
-    s3.upload(params, (err, data) => {
-      if (err) {
-        console.error("Error uploading image:", err);
-      } else {
-        console.log("Image uploaded successfully:", data.Location);
-      }
+export function handleUploadClick(file) {
+    AWS.config.update({
+      accessKeyId: 'YOUR_AWS_ACCESS_KEY_ID',
+      secretAccessKey: 'AWS_SECRET_ACCESS_KEY',
+      region: 'YOUR_AWS_REGION'
     });
-  }
+
+    const fileInput = document.getElementById("fileInput");
+    let filename = file
+
+    if (fileInput.files.length > 0) {
+        const selectedFile = fileInput.files[0];
+        console.log(selectedFile.type);
+        uploadFile(selectedFile);
+    } else {
+        alert("Select File and try again!");
+    }
+
+    function uploadFile(selectedFile) {
+        const s3 = new AWS.S3();
+        let type = "";
+
+        if (selectedFile.type === "image/jpeg") {
+            type = ".jpg";
+        } else if (selectedFile.type === "image/png") {
+            type = ".png";
+        }
+        // else if (selectedFile.type === "image/svg+xml") {
+        //   type = ".svg";
+        // } 
+        else if (selectedFile.type === "image/gif") {
+            type = ".gif";
+        }
+
+        if (filename === "") {
+            filename = selectedFile.name;
+        }
+
+        if (type === '.gif') {
+            const params = {
+                Bucket: "imageproject101",
+                Key: filename + type,
+                Body: selectedFile,
+                ContentType: selectedFile.type,
+                ContentDisposition: 'attachment'
+            };
+            s3.upload(params, (err, data) => {
+                if (err) {
+                    console.error("Error uploading image:", err);
+                } else {
+                    console.log("Image uploaded successfully:", data.Location);
+                    alert("Image Uploaded Successfully");
+                    // You can display the uploaded image or perform other actions here
+                }
+            });
+        }
+        else {
+            const params = {
+                Bucket: "imageproject101",
+                Key: filename + type,
+                Body: selectedFile,
+                ContentDisposition: 'attachment'
+            };
+            s3.upload(params, (err, data) => {
+                if (err) {
+                    console.error("Error uploading image:", err);
+                } else {
+                    console.log("Image uploaded successfully:", data.Location);
+                    alert("Image Uploaded Successfully");
+                }
+            });
+
+        }
+
+    }
 }
